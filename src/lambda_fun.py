7db770b291df58c2b421f2768ee061525347ae21
wray <wray@wrayesian.com>
2017-03-28 10:22:38 -0400

    Initial cut at a 'voice home page' for a small business

--- src/lambda_fun.py
+++ src/lambda_fun.py
@@ -1,13 +1,20 @@
 """
-This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
-The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well
-as testing instructions are located at http://amzn.to/1LzFrj6
+Simple Python Lambda service providing example for a small business "voice site". You know,
+this is like the Alexa version of your web site.
 
-For additional samples, visit the Alexa Skills Kit Getting Started guide at
-http://amzn.to/1LGWsLG
+Intents supported:
+  Amazon.HelpIntent
+  About
+  Contact
+  Upcoming
+
+
 """
 
-from __future__ import print_function
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 # --------------- Helpers that build all of the responses ----------------------
@@ -15,13 +22,13 @@ from __future__ import print_function
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
     return {
         'outputSpeech': {
-            'type': 'PlainText',
-            'text': output
+            'type': 'SSML',
+            'ssml': output
         },
         'card': {
             'type': 'Simple',
-            'title': "SessionSpeechlet - " + title,
-            'content': "SessionSpeechlet - " + output
+            'title': title,
+            'content': output
         },
         'reprompt': {
             'outputSpeech': {
@@ -41,97 +48,61 @@ def build_response(session_attributes, speechlet_response):
     }
 
 
-# --------------- Functions that control the skill's behavior ------------------
+# --------------- Your functions to implement your intents ------------------
 
-def get_welcome_response():
-    """ If we wanted to initialize the session to have some attributes we could
-    add those here
-    """
-
+def about(intent, session):
     session_attributes = {}
-    card_title = "Welcome"
-    speech_output = "Welcome to the Alexa Skills Kit sample. " \
-                    "Please tell me your favorite color by saying, " \
-                    "my favorite color is red"
-    # If the user either does not reply to the welcome message or says something
-    # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Please tell me your favorite color by saying, " \
-                    "my favorite color is red."
+    reprompt_text = None
+    speech_output = ""
     should_end_session = False
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+
+    speech_output = "<speak>Welcome to ACME Inc. We are the coolest company located in the Silicon Valley of the South. We love our employees, customers, and the environment.</speak>"
+
+    return build_response(session_attributes, build_speechlet_response
+                          (intent['name'], speech_output, reprompt_text, should_end_session))
+
+def contact(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+    speech_output = ""
+    should_end_session = False
+
+    speech_output = "<speak>The best way to reach us is at info at acme dot com. You can also leave us voice mail at 8 0 4, 5 5 5, 1 2 1 2. We are also on twitter, at acme S V O S.</speak>"
+
+    return build_response(session_attributes, build_speechlet_response
+                          (intent['name'], speech_output, remprompt_text, should_end_session))
+
+def upcoming(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+    speech_output = ""
+    should_end_session = False
+
+    speech_output = "<speak>Check us out at Stir Trek dot com. We will have a booth and speakers in Columbus, Ohio on May  5th!</speak>"
+
+    return build_response(session_attributes, build_speechlet_response
+                          (intent['name'], speech_output, remprompt_text, should_end_session))
 
 
 def handle_session_end_request():
     card_title = "Session Ended"
-    speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
-                    "Have a nice day! "
+    speech_output = "Thank you for asking about our business. " \
+      "Have a nice day! "
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
 
-def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
 
 
-def set_color_in_session(intent, session):
-    """ Sets the color in the session and prepares the speech to reply to the
-    user.
-    """
-
-    card_title = intent['name']
-    session_attributes = {}
-    should_end_session = False
-
-    if 'Color' in intent['slots']:
-        favorite_color = intent['slots']['Color']['value']
-        session_attributes = create_favorite_color_attributes(favorite_color)
-        speech_output = "I now know your favorite color is " + \
-                        favorite_color + \
-                        ". You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-        reprompt_text = "You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "Please try again."
-        reprompt_text = "I'm not sure what your favorite color is. " \
-                        "You can tell me your favorite color by saying, " \
-                        "my favorite color is red."
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-
-
-def get_color_from_session(intent, session):
-    session_attributes = {}
-    reprompt_text = None
-
-    if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
-        favorite_color = session['attributes']['favoriteColor']
-        speech_output = "Your favorite color is " + favorite_color + \
-                        ". Goodbye."
-        should_end_session = True
-    else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "You can say, my favorite color is red."
-        should_end_session = False
-
-    # Setting reprompt_text to None signifies that we do not want to reprompt
-    # the user. If the user does not respond or says something that is not
-    # understood, the session will end.
-    return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
-
-
-# --------------- Events ------------------
+# --------------- Primary Events ------------------
 
 def on_session_started(session_started_request, session):
     """ Called when the session starts """
 
-    print("on_session_started requestId=" + session_started_request['requestId']
-          + ", sessionId=" + session['sessionId'])
+    logger.info("on_session_started requestId=" + session_started_request['requestId'] +
+                ", sessionId=" + session['sessionId'])
 
 
 def on_launch(launch_request, session):
@@ -139,28 +110,40 @@ def on_launch(launch_request, session):
     want
     """
 
-    print("on_launch requestId=" + launch_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
+    logger.info("on_launch requestId=" + launch_request['requestId'] +
+                ", sessionId=" + session['sessionId'])
     # Dispatch to your skill's launch
-    return get_welcome_response()
+    return build_response({},build_speechlet_response(
+        "ACME Inc.", "<speak>Welcome to the 411 for ACME Inc. This skill provides information about ACME, a really cool company that Alexa loves to talk about.</speak>","",False))
+
+
+def get_help():
+    """ Called when the user asks for help
+    """
+
+    return build_response({},build_speechlet_response(
+        "ACME Inc.","""<speak>This skill provides some basic information about ACME. You can ask for our location, contact info, and upcoming events.</speak>""","",False)) 
 
 
 def on_intent(intent_request, session):
     """ Called when the user specifies an intent for this skill """
 
-    print("on_intent requestId=" + intent_request['requestId'] +
-          ", sessionId=" + session['sessionId'])
+    logger.info("on_intent requestId=" + intent_request['requestId'] +
+                ", sessionId=" + session['sessionId'])
 
     intent = intent_request['intent']
     intent_name = intent_request['intent']['name']
 
     # Dispatch to your skill's intent handlers
-    if intent_name == "MyColorIsIntent":
-        return set_color_in_session(intent, session)
-    elif intent_name == "WhatsMyColorIntent":
-        return get_color_from_session(intent, session)
+
+    if intent_name == "About":
+        return about(intent, session)
+    elif intent_name == "Contact":
+        return contact(intent, session)
+    elif intent_name == "Upcoming":
+        return upcoming(intent,session)    
     elif intent_name == "AMAZON.HelpIntent":
-        return get_welcome_response()
+        return get_help()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
         return handle_session_end_request()
     else:
@@ -183,7 +166,7 @@ def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
     etc.) The JSON body of the request is provided in the event parameter.
     """
-    print("event.session.application.applicationId=" +
+    logger.info("event.session.application.applicationId=" +
           event['session']['application']['applicationId'])
 
     """
@@ -203,5 +186,5 @@ def lambda_handler(event, context):
         return on_launch(event['request'], event['session'])
     elif event['request']['type'] == "IntentRequest":
         return on_intent(event['request'], event['session'])
-    elif event['request']['type'] == "SessionEndedRequest":
+    else:
         return on_session_ended(event['request'], event['session'])
