18e856655661d0610dfc7124f057a54c30b5f3fd
wray <wray@wrayesian.com>
2017-04-26 18:19:28 -0400

    Adding comments and improved README

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
-# alexa
+# Build an Alexa Skill from a JSON file
+
+## What You Will Learn
+
+This repo contains several branches with different python-based lambda
+services and related skills intents and utterances  that fulfill a basic facts-oriented skill. The example use
+case provides a simple Voice User Interface presence for a small
+business. You know, just like every business needs a website, now
+every business needs an Alexa Skill (Voice Site).
+
+* Introduction to building an Alexa Skill without getting too much
+into code.
+* Voice User Interface (VUI) design
+* SSML
+* Lambda Services
+* S3
+* Optional Github to Cloud deployment via Travis
+
+## What You Will Need
+
+* [Amazon Developer Portal Account](http://developer.amazon.com)
+* [Amazon Web Services Account](http://aws.amazon.com/)
+* The sample code on the s3 branch on
+[GitHub](https://github.com/techemstudios/alexa_python).
+
+## What Your Skill Will Do
+
+An Alexa skil built using a Python lambda function that fetchs a JSON
+file from S3 that encodes the responses for your intents.
+
+Suppose you name your skill ACME Inc., you will be able to interact
+with your skill with phrases like, "Alexa, ask ACME Inc. for contact
+info" or "Alexa, ask ACME Inc. for their upcoming events." or "Alexa,
+ask ACME Inc. for general info about ACME." or "Alexa, tell me about
+ACME Inc."
+
+## Make it Your Own
+
+By simply copying and pasting the
+intentSchema and Utterances into the Skill builder, you can create the
+lambda, copy and paste the code into your configured lambda, give the
+lambda role read permissions to your S3, create the "alexa-python-biz"
+bucket and upload the "discplines.json" there and you can quickly have
+a working skill.
+
+You can then easily update the responses in your JSON file, upload
+again to S3 and modify your skill.
+
+Update the invocation name to a different business and match your
+response to provide relevant information for the business.
+
+But, wait, that's not all. Go ahead and completely change the skill to
+a different set of information for something different. Change your
+invocation to brew facts and create different/new intents and
+utterances to for randomFacts, localBeers, etc. And then you'll
+quickly be ready to start using parameters to make things even more
+dynamic (watch here for the parameter version).
+
+So, as long as you keep your JSON intents in sync with your skill intentSchema, you can
+simply update or add intents to the JSON file in S3 and the lambda service will use them.
+Intents in your Schema may be mixed case -- this code will convert to lower case.
+
+### Cloud CI/CD with Travis
+
+This repo includes a .travis.yml file ready to be updated to
+facilitate updating your lambda and your JSON as you make changes and
+push to github.
+
+If you fork this repo or create your own copy and keep it as a public repo, you can use
+Travis to deploy to your lambda/S3. You'll want to change the following configs:
+
+* in deploy-provider: lambda
+  * function_name
+  * role
+  * access_key_id (available from AWS console)
+  * secret_access_key (also available from AWS console, but make sure you use travis command
+  * line to encrypt your key)
+
+* in deploy-provider: s3
+  * bucket (if you have changed it)
+  * access_key_id (can use the same one as above)
+  * secret_access_key (also use the same one as above)
--- src/alexa_py.py
+++ src/alexa_py.py
@@ -1,15 +1,48 @@
 """
-Simple Python Lambda service providing example for a small business "voice site". You know,
-this is like the Alexa version of your web site.
+Simple Python Lambda service that uses response.json file in an S3 bucket to provide simple
+responses to simple "fact"-like intents. The example response.json with this repo supports
+thes following:
 
 Intents supported:
-  Amazon.HelpIntent
-  About
-  Contact
-  Upcoming
 
+  Custom:
+    About
+    Contact
+    Upcoming
+
+  Required:
+    LaunchRequest (request type)
+    AMAZON.HelpIntent (intent)
+    AMAZON.CancelIntent or AMAZON.StopIntent (intent, both use 'end' response)
+
+Note, that as long as you keep your intents in sync with your skill intentSchema, you can
+simply update or add intents to the JSON file in S3 and the lambda service will use them.
+Intents in your Schema may be mixed case -- this code will convert to lower case.
+
+Further note, there is .travis.yml in this repo that does two things:
+  1) Deploys this code to your configured lambda function.
+  2) Deploys the ../responses/response.json to your bucket.
+
+If you fork this repo or create your own copy and keep it as a public repo, you can use
+Travis to deploy to your lambda/S3. You'll want to change the following configs:
+
+  in deploy-provider: lambda
+    function_name
+    role
+    access_key_id (available from AWS console)
+    secret_access_key (also available from AWS console, but make sure you use travis command
+    line to encrypt your key)
+
+  in deploy-provider: s3
+    bucket (if you have changed it)
+    access_key_id (can use the same one as above)
+    secret_access_key (also use the same one as above)
 
 """
+
+# If you want to use a different bucket name or response JSON file, change them here
+BUCKET_NAME='alexa-python-biz'
+RESPONSE_JSON='response.json'
 
 import logging
 import boto3
@@ -20,7 +53,8 @@ logger.setLevel(logging.INFO)
 
 # Load the S3 JSON
 s3 = boto3.resource('s3')
-json_string = s3.Object('alexa-python-biz','response.json').get()['Body'].read().decode('utf-8')
+# Grab the JSON from the file and load it into a map
+json_string = s3.Object(BUCKET_NAME,RESPONSE_JSON).get()['Body'].read().decode('utf-8')
 responses = json.loads(json_string)
 
 
@@ -55,6 +89,9 @@ def build_response(session_attributes, speechlet_response):
     }
 
 
+# Function which delegates the speech output for the response based on the JSON file.
+# Simply looking up the intent in the responses map created from the parsed JSON.
+#
 def on_intent(intent_request, session):
     """ Called when the user specifies an intent for this skill """
 
@@ -65,33 +102,39 @@ def on_intent(intent_request, session):
     intent_name = intent_request['intent']['name']
 
     # Dispatch to your skill's intent handlers
-
-    session_attributes = {}
-    reprompt_text = None
+    session_attributes = {} # No session attributes needed for simple fact responses
+    reprompt_text = None # No reprompt text set
     speech_output = ""
-    should_end_session = True
+    should_end_session = True # Can end session after fact is returned (no additional dialogue)
 
     if intent_name == 'launch':
-        should_end_session = False
+        should_end_session = False # Opening a skill requires the session remain open
     elif intent_name == "AMAZON.HelpIntent":
-        should_end_session = False
+        should_end_session = False # Asking for help requires the session remain open
         intent_name = 'help'
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
         intent_name = 'end'
     else:
         intent_name = intent_name.lower()
 
+    # Grab the response specified for the given intent in the JSON
     speech_output = responses[intent_name]
 
     return build_response(session_attributes, build_speechlet_response
                           (intent_name,speech_output,reprompt_text,should_end_session))
 
+
 # --------------- Main handler ------------------
 
+# This is the function called when the lambda is invoked by the skill.
+# Essentially, JSON is passed in from ASK and your lambda must prepare the response JSON
+#
 def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
     etc.) The JSON body of the request is provided in the event parameter.
     """
+    # Logging a message to CloudWatch to help with debugging once you are testing the lambda
+    # and the skill that calls it.
     logger.info("event.session.application.applicationId=" +
           event['session']['application']['applicationId'])
 
@@ -104,6 +147,8 @@ def lambda_handler(event, context):
     #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
     #     raise ValueError("Invalid Application ID")
 
+    # I am injecting a new "intent" type of launch in order to
+    # allow the JSON to provide the response text for a LaunchRequest
     if event['request']['type'] == "LaunchRequest":
         event['request']['intent'] = { 'name':'launch' }
     
